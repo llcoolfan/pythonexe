bf2a2b9e5fae485dce218fa244cd46025c78d167
Jijia <jiabin_fan@163.com>
2020-03-17 20:25:23 +0800

    modified pythontest add datatime print

--- pythontest.py
+++ pythontest.py
@@ -2,7 +2,10 @@
 # -*- coding=utf-8 -*-
 # Author:llcoolf
 
+import datetime
+
 if __name__ == "__main__":
+    print(datetime.datetime.now())
     print("hello world test")
     print("Use Git as version control system")
     print("git is great")
