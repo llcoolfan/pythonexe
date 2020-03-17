2dfd8a81dc8d921e45883386ede4a0339aea0f15
Jijia <jiabin_fan@163.com>
2020-03-17 20:58:34 +0800

    add datetime print function

--- pythontest.py
+++ pythontest.py
@@ -7,6 +7,6 @@ import datetime
 if __name__ == "__main__":
     print(datetime.datetime.now())
     print("hello world test")
-    print("Use Git as version control system")
+    print("Use Git as version control system!")
     print("git is great")
     print("just add one line right now")
